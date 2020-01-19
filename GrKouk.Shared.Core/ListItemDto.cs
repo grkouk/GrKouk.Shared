018fbbbb0733f35a77dd1bf02462e2ed32bb08d5
GrKouk <grkouk@hotmail.com>
2020-01-19 15:18:24 +0200

    Changes

---
+++ GrKouk.Shared.Core/ListItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GrKouk.Shared.Core
+{
+    public class ListItemDto
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemCode { get; set; }
+    }
+}
---
+++ GrKouk.Shared.Core/SearchListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace GrKouk.Shared.Core
+{
+    public class SearchListItem
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+    }
+}
