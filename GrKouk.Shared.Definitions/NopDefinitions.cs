451a5b44611183b741888d62c97642f8b0d3cbe1
GrKouk <grkouk@hotmail.com>
2020-01-20 18:21:32 +0200

    Changes

---
+++ GrKouk.Shared.Definitions/NopDefinitions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace GrKouk.Shared.Definitions
+{
+    public enum ShopEnum
+    {
+        [Description("Angelikas Creations")] ShopAngelikasCreations = 1,
+        [Description("Handmade Creations")] ShopHandmadeCreations = 2,
+        [Description("To Braxiolaki")] ShopToBraxiolaki = 3
+    }
+}
